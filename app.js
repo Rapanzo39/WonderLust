65578e65d12133d157ff45d43e8683f5cf3780ee
Kaushalya Shahapure <kaushalyashahapure258@gmail.com>
2026-06-13 18:04:03 +0530

    Added new features and updated project

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const app = express();
 const Listing = require('./models/listing');
 const initData = require('./init/data.js');
 const path = require('path');
+const methodOverride = require('method-override');
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 const MONGO_URI = 'mongodb://localhost:27017/wonderlust';
 
@@ -54,6 +58,20 @@ app.post('/listings',async(req,res)=>{
    // console.log(newListing);
     res.redirect("listings");
    
+});
+
+//edit route
+app.get('/listings/:id/edit',async(req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render('listing/edit',{listing});
+});
+
+//update route
+app.put('/listings/:id',async(req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{... req.body.listing });
+    res.redirect(`/listings/${id}`);
 });
 
 
